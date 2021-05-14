c5d6f53bbef44905e59e052a3673427ca361f923
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-13 23:38:07 -0300

    routes primárias de curso funcionando

--- routes/routes.js
+++ routes/routes.js
@@ -5,3 +5,7 @@ const curso = require("../controllers/CursoController")
 
 router.post("/espaco", espaco.create)
 router.post("/curso", curso.create)
+router.get("/curso", curso.index)
+router.get("/curso/:id", curso.findCurso)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var app = require("express")()
 var router = require("./routes/routes")
+var bodyParser = require("body-parser")
 
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+//parse application/json
+app.use(bodyParser.json())
 app.use("/", router)
 
 app.listen(3000, () => {
