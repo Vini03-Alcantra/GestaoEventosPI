a30079cd40974e3c4905c6bcc674a71817a2d912
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-18 22:07:28 -0300

    Criando alunocontroller

--- controllers/AlunoController.js
+++ controllers/AlunoController.js
@@ -8,7 +8,52 @@ class AlunoController {
 
     async findLocal(req, res){
         var id = req.params.id;
-        var espaco = await Aluno.findByI
+        var aluno = await Aluno.findById(id)
+        if (aluno != null || aluno != []) {
+            res.statusCode = 200;
+            res.json(aluno)
+        } else {
+            res.statusCode = 404;
+            res.json({})
+        }
+    }
+
+    async create(req, res){
+        var {NomeAluno, emailAluno, MatriculaAluno, CpfAluno, password} = req.body;
+                    
+        var aluno = await Aluno.new(NomeAluno, emailAluno, MatriculaAluno, CpfAluno, password)
+        res.statusCode = 200;
+        res.json(aluno)
+        
+    }
+
+    async edit(req, res){
+        var {idAluno, NomeAluno, CpfAluno} = req.body;
+        var result = await Aluno.update(idAluno, NomeAluno, CpfAluno)
+        if (result != undefined) {
+            if (result.status) {
+                res.statusCode = 200
+                res.json(result)
+            } else {
+                res.statusCode = 406
+                res.json(result)
+            }
+        } else {
+            res.statusCode = 406
+            res.json({})    
+        }
+    }
+
+    async remove(req, res){
+        var id = req.params.id
+        var result = await Aluno.delete(id)
+        if (result.status) {
+            res.statusCode = 200
+            res.json(result)
+        } else {
+            res.statusCode = 406
+            res.json({})
+        }
     }
 }
 
